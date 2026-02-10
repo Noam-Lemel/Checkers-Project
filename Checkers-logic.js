c6d04b78e97524913fb995da9d6d4cef8fe310bc
Noam Lemel <noam123435@gmail.com>
2026-02-11 00:28:05 +0200

    Add files via upload

---
+++ Checkers-logic.js
@@ -0,0 +1,234 @@
+//Global var
+let isLock=false;
+let turnCount=1;
+let turnOnlyKingsMove=0;
+let isMultipleCapture=false;
+let isWhiteTurn=(turnCount%2===0);
+const switchTurn=()=>{
+     turnCount++;
+    isWhiteTurn=(turnCount%2===0);
+}
+const getIsWhiteTurn=()=>{
+    return isWhiteTurn;
+}
+//turn flow
+const handleMove=(fromID,toID,isKing,isTargetSquareEmpty,playerColor,enemyID,enemyColor,pathId,pathData)=>{
+    //the king's logic
+    if(isKing){
+        if(isValidKingMove(fromID,toID,isTargetSquareEmpty)){
+            const result=isValidKingPath(pathId,pathData);
+            if(result==='move'){
+                const playerStatus=getPlayerStatus(playerColor);
+                 move(fromID,toID);
+                 if(playerStatus.canCapture.length>0){
+                    isLock=true;
+                    setTimeout(()=>{
+                    move(toID,fromID)
+                    setTimeout(()=>{
+                    burnPieces(playerStatus.canCapture);
+                    finishTurn(fromID)
+                    isLock=false;
+                    },500)
+                },500)}
+                    else{
+                    turnOnlyKingsMove++;
+                    finishTurn(toID);}}
+            else if(result===false)
+                   resetSelectedSquare();
+            else{
+                    capture(fromID,toID,result);
+                    if(isKingCanCaptureFromCurrentSpot(toID,enemyColor)){
+                        resetSelectedSquare();
+                        setSelectedSquare(toID);
+                        isMultipleCapture=true;
+                    }else{
+                    isMultipleCapture=false;
+                    finishTurn(toID);
+                    turnOnlyKingsMove=0;
+                }}
+            }
+        else
+            resetSelectedSquare();
+    }
+    //the man's logic
+    else if(isValidManMove(fromID,toID,isTargetSquareEmpty)){
+            const playerStatus=getPlayerStatus(isWhiteTurn?'white':'red');
+            move(fromID,toID);
+            if(playerStatus.canCapture.length>0){
+                isLock=true;
+                setTimeout(()=>{
+                    move(toID,fromID)
+                    setTimeout(()=>{
+                         burnPieces(playerStatus.canCapture);
+                        finishTurn(fromID)
+                        turnOnlyKingsMove=0;
+                        isLock=false;
+                    },500)
+                },500)
+                }else
+                    finishTurn(toID);
+                    turnOnlyKingsMove=0;
+                }
+     else if(isValidCapture(fromID,toID,isTargetSquareEmpty,playerColor,enemyColor)){
+        capture(fromID,toID,enemyID);
+        if(isManCanCaptureFromCurrentSpot(toID,enemyColor)){
+            resetSelectedSquare();
+            setSelectedSquare(toID);
+            isMultipleCapture=true;
+        }
+       else{
+            isMultipleCapture=false;
+            turnOnlyKingsMove=0;
+            finishTurn(toID);}
+     }
+    else{
+       resetSelectedSquare();
+    }
+}
+//the man's logic
+const isValidManMove=(fromID,toID,isTargetSquareEmpty)=>{
+    const dif=parseInt(toID-fromID);
+    const fromCol=((parseInt(fromID)-1)%8);
+    const toCol=((parseInt(toID)-1)%8);
+    const colDif=fromCol-toCol;
+    const isRightDirection=isWhiteTurn?((dif===7||dif===9)):(dif===-7||dif===-9);
+    if(toID>64||toID<0||!isTargetSquareEmpty)
+        return false;
+    if(isRightDirection){
+        if((colDif===1||colDif===-1))
+                return true;
+    }
+    return false;
+    }
+const isValidCapture=(fromID,toID,isTargetSquareEmpty,playerColor,enemyColor)=>{
+    const from=parseInt(fromID);
+    const to=parseInt(toID);
+    if(toID>64||toID<0||!isTargetSquareEmpty)
+        return false;
+    const fromCol=parseInt((from-1)%8);
+    const toCol=parseInt((to-1)%8);
+    const colDif=fromCol>toCol?fromCol-toCol:toCol-fromCol;
+    const fromRow=parseInt((from-1)/8);
+    const toRow=parseInt((to-1)/8);
+    const rowDif=fromRow>toRow?fromRow-toRow:toRow-fromRow;
+     if(colDif!==2||rowDif!==2)
+        return false; 
+    if(enemyColor!==""&&playerColor!==enemyColor)
+        return true;
+    return false;
+    }
+    const getEnemyId=(fromID,toID)=>{
+    const from=parseInt(fromID);
+    const to=parseInt(toID);
+    const dif=to-from;
+    if(dif===14||dif===18||dif===-14||dif===-18)
+        return (from+to)/2;
+    else
+        return null;
+}
+    //the king's logic
+const isValidKingMove=(fromID,toID,isTargetSquareEmpty)=>{
+    const fromCol=((parseInt(fromID)-1)%8);
+    const toCol=((parseInt(toID)-1)%8);
+    const colDif=(fromCol>toCol)?(fromCol-toCol):(toCol-fromCol);
+    const fromRow=parseInt((fromID-1)/8);
+    const toRow=parseInt((toID-1)/8);
+    const rowDif=(fromRow>toRow)?(fromRow-toRow):(toRow-fromRow);
+
+    if(colDif!==rowDif)return false;
+    if(!isTargetSquareEmpty)return false;
+    return true;
+}
+const isValidKingPath=(pathId,pathData)=>{
+    let enemyPieceCounter=0;
+    let enemyPiece=null;
+    if(pathData[pathData.length-1]!=='empty') return false;
+     for(let i=0;i<pathData.length-1&&(enemyPieceCounter<2);i++){
+        const squareInfo=pathData[i];
+        if(squareInfo==='empty')continue;
+        if(squareInfo==='self')return false;
+            else{
+                enemyPiece=pathId[i];
+                enemyPieceCounter++;
+            }
+    }
+    if(enemyPieceCounter===0)return 'move';
+    if(enemyPieceCounter===1) return enemyPiece;
+    return false;
+}
+const getPathId=(fromID,toID)=>{
+    const fromRow=parseInt((fromID-1)/8);
+    const toRow=parseInt((toID-1)/8);
+    const rowDif=(fromRow>toRow)?(fromRow-toRow):(toRow-fromRow);
+    const dif=parseInt(toID-fromID);
+    const step=parseInt(dif/rowDif);
+    const fromCol=parseInt((fromID-1)%8);
+    const toCol=parseInt((toID-1)%8);
+    const colDif=(fromCol>toCol)?(fromCol-toCol):(toCol-fromCol);
+    let pathId=[];
+    if(colDif!==rowDif) return pathId;
+     for(let current=parseInt(fromID)+step;;current+=step){
+        pathId.push(current);
+        if(current===parseInt(toID)) break;
+     }
+     return pathId;
+}
+const isManCanCaptureFromCurrentSpot=(fromID,enemyColor)=>{
+    const options=[14,18,-14,-18];
+    let idNumber=parseInt(fromID);
+    const fromCol=parseInt((idNumber-1)%8);
+    for(let option of options){
+        const toID=idNumber+option;
+        const midID=parseInt(idNumber+(option/2));
+        const toCol=parseInt((toID-1)%8);
+        const colDif=fromCol>toCol?fromCol-toCol:toCol-fromCol;
+        if(toID>64||toID<0) continue;
+        if(colDif!==2) continue;
+        if(getSquareStatus(midID)===enemyColor&&getSquareStatus(toID)==="empty") return true;
+    }
+    return false;
+}
+const isKingCanCaptureFromCurrentSpot=(fromID,enemyColor)=>{
+    let idNumber=parseInt(fromID);
+    const options=[7,9,-7,-9];
+    for(let option of options){
+        let nextId=idNumber+option;
+        let fromCol=parseInt((idNumber-1)%8);
+        let foundPiece=false;
+        while((nextId>0)&&(nextId<=64)){
+            let toCol=parseInt((nextId-1)%8);
+            let colDif=(fromCol>toCol)?(fromCol-toCol):(toCol-fromCol);
+            if(colDif!==1)break;
+            const nextSquare=getSquareStatus(nextId);
+            if(nextSquare==='empty'){
+                if(foundPiece)return true;}
+            else if(nextSquare===enemyColor){
+                if(foundPiece) break;
+                foundPiece=true;
+            }else 
+                break;
+            nextId+=option;
+            fromCol=toCol;
+        }
+    }
+    return false;
+}
+const isOneStepValid=(fromID,toID,isKing,color)=>{
+    if(toID<1||toID>64)return false;
+    const fromCol=((parseInt(fromID)-1)%8);
+    const toCol=((parseInt(toID)-1)%8);
+    const colDif=fromCol>toCol?fromCol-toCol:toCol-fromCol;
+    const dif=toID-fromID;
+    const isRightDirection=(color==='white')?dif<0:dif>0
+    if(colDif!==1) return false;
+    if(!isKing&&!isRightDirection) return false;
+    return true;
+}
+const isVictory=()=>{
+    const playerStatus=getPlayerStatus((isWhiteTurn?"red":"white"));
+    if(playerStatus.numberOfPieces===0)
+        return true;
+    if(playerStatus.canMove===false && playerStatus.canCapture.length===0)
+        return true;
+    return false;
+}
---
+++ Checkers-ui.js
@@ -0,0 +1,248 @@
+//DOM Objects
+const board=document.getElementById('board');
+const buttonResign=document.getElementById('button-resign');
+const buttonDraw=document.getElementById('button-draw');
+const modalContainer=document.getElementById('modal-container');
+const modal=document.getElementById('modal');
+const modalMessage=document.getElementById('modal-message');
+const yesButton=document.getElementById('yes-button');
+const noButton=document.getElementById('no-button');
+const turnmessage=document.getElementById('turn-message');
+turnmessage.innerHTML=`${getIsWhiteTurn()?'white':'red'} turn`;
+//Global var
+let victory=false;
+let draw=false;
+let selectedSquare=null;
+let modalMission='';
+//Create board
+    let squareCounter=1;
+    for(let row=0;row<8;row++){
+      for(let col=0;col<8;col++){
+        const square=document.createElement('div');
+        square.classList.add('square');
+        square.id=squareCounter;
+        if(row%2===0){
+           square.classList.add(col%2===0?'white':'black'); 
+        }
+        else if(row%2!==0){
+            square.classList.add(col%2===0?'black':'white');
+        }
+        square.addEventListener('click',(event)=>{
+            if(isLock)
+                return;
+            if(selectedSquare===null)
+                return;
+                let isTargetSquareEmpty=square.children.length===0;
+                let playerColor=selectedSquare.children[0].classList.contains('white')?'white':'red';
+                let enemyColor="";
+                let enemyID=getEnemyId(selectedSquare.id,square.id);
+                let enemySquare=null;
+                let isKing=selectedSquare.children[0].classList.contains('king');
+                let pathId=[];
+                let pathData=[];
+                if(isKing){
+                    pathId=getPathId(selectedSquare.id,square.id);
+                if(!pathId){
+                    resetSelectedSquare();
+                    return;
+                }
+                pathData=getPathData(pathId,playerColor);
+                if(!pathData){
+                    resetSelectedSquare();
+                    return;
+                }}
+                if(enemyID){
+                enemySquare=document.getElementById(enemyID);
+                if(enemySquare&&enemySquare.children.length>0)
+                enemyColor=enemySquare.children[0].classList.contains('white')?'white':'red';
+                }
+                if(isMultipleCapture){
+                    if(isKing){
+                        if(typeof(isValidKingPath(pathId,pathData))==='number')
+                            handleMove(selectedSquare.id,square.id,isKing,isTargetSquareEmpty,playerColor,enemyID,enemyColor,pathId,pathData);
+                    }
+                    else if(isValidCapture(selectedSquare.id,square.id,isTargetSquareEmpty,playerColor,enemyColor))
+                        handleMove(selectedSquare.id,square.id,isKing,isTargetSquareEmpty,playerColor,enemyID,enemyColor,pathId,pathData);
+                }else
+                     handleMove(selectedSquare.id,square.id,isKing,isTargetSquareEmpty,playerColor,enemyID,enemyColor,pathId,pathData);
+        })
+        board.appendChild(square);
+        squareCounter++;
+    }
+    const getPathData=(pathId,playerColor)=>{
+        const pathData=[];
+        if(pathId.length===0)return pathData;
+        for(let i=0;i<pathId.length;i++){
+            const square=document.getElementById(pathId[i]);
+            let squareInfo='';
+            if(square.children.length===0)
+                squareInfo='empty';
+            else 
+                squareInfo=(square.children[0].classList.contains(playerColor))?'self':'enemy';
+            pathData.push(squareInfo);
+        }
+        return pathData;
+    }
+}//Pieces Injection
+    const squares=document.querySelectorAll('#board div');
+    for(square of squares){
+        const piece=document.createElement('div');
+        if(parseInt(square.id)<=24&&square.classList.contains('black')){
+            piece.classList.add('piece','white');
+            square.appendChild(piece);
+        }
+        else if(parseInt(square.id)>=41&&square.classList.contains('black')){
+            piece.classList.add('piece','red');
+            square.appendChild(piece);
+        }
+        if(piece.classList.contains('piece')){
+            piece.addEventListener('click',(event)=>{
+                event.stopPropagation()
+                if(isLock)
+                    return;
+                if(selectedSquare===null){
+                if((getIsWhiteTurn() && event.target.classList.contains('white'))||(!getIsWhiteTurn() && event.target.classList.contains('red'))){
+                event.target.parentElement.classList.add('selected');
+                selectedSquare=event.target.parentElement;}}
+            })
+        }
+    }
+    const getSquareStatus=(id)=>{
+        const square=document.getElementById(id);
+        if(!square) return 'out';
+        if(square.children.length===0) return 'empty';
+        return square.children[0].classList.contains('white')?'white':'red';
+    }
+     //parctical actions
+const move=(fromID,toID)=>{
+const piece=document.getElementById(fromID).children[0];
+ const toSquare=document.getElementById(toID);
+ toSquare.appendChild(piece);
+}
+const capture=(fromID,toID,enemyID=null)=>{
+    const from=parseInt(fromID);
+    const to=parseInt(toID);
+    if(!enemyID)
+        enemyID=(to+from)/2;
+    const enemySquare=document.getElementById(enemyID);
+    enemySquare.innerHTML="";
+    move(fromID,toID);
+}
+const resetSelectedSquare=()=>{
+    if(selectedSquare){
+         selectedSquare.classList.remove('selected');
+            selectedSquare=null;
+    }   
+}
+const setSelectedSquare=(id)=>{
+    const newsSlectedSquare=document.getElementById(id);
+            selectedSquare=newsSlectedSquare;
+            selectedSquare.classList.add('selected');
+}
+const finishTurn=(toID)=>{
+     const toSquare=document.getElementById(toID);
+     if(toSquare.children.length>0){
+    const piece=toSquare.children[0];
+     if((piece.classList.contains('white')&&toID>56)||(piece.classList.contains('red')&&toID<9))
+     piece.classList.add('king');}
+    resetSelectedSquare();
+    victory=isVictory();
+    draw=(turnOnlyKingsMove>=30);
+    if(victory||draw){
+        endGame(victory,draw);
+        return;
+    }
+   switchTurn();
+   UpdateTurn();
+    victory=isVictory()
+    if(victory)
+        endGame(victory,draw);
+}
+const UpdateTurn=()=>{
+    turnmessage.innerHTML=`${getIsWhiteTurn()?'white':'red'} turn`;
+    turnmessage.classList.remove('red','white');
+    turnmessage.classList.add(getIsWhiteTurn()?'white':'red');
+}
+const getPlayerStatus=(color)=>{
+    const squares=document.querySelectorAll('.square');
+    const options=[7,9,-7,-9];
+    const playerStatus={
+        canCapture:[],
+        canMove:false,
+        numberOfPieces:0
+    }
+    for(let square of squares){
+        if(square.children.length===0)
+            continue;
+        const piece=square.children[0];
+        let isKing=piece.classList.contains('king');
+        if(piece.classList.contains(color)){
+            playerStatus.numberOfPieces++;
+            if(piece.classList.contains('king')){
+                if(isKingCanCaptureFromCurrentSpot(square.id,color==='white'?'red':'white')){
+                    playerStatus.canCapture.push(square.id);
+                    playerStatus.canMove=true;}
+            }else{
+                if(isManCanCaptureFromCurrentSpot(square.id,color==='white'?'red':'white')){
+                    playerStatus.canCapture.push(square.id);
+                      playerStatus.canMove=true;}
+            }
+            for(let option of options){
+            let optionId=parseInt(square.id)+option;
+            if(isOneStepValid(parseInt(square.id),optionId,isKing,color)){
+                let optionSquare=document.getElementById(optionId);
+            if(optionSquare.children.length===0)
+                playerStatus.canMove=true;}
+            }
+        }
+    }
+    return playerStatus;
+}
+const burnPieces=(squaresId)=>{
+    for(let squareId of squaresId){
+        const square=document.getElementById(squareId);
+        if(square.children.length>0)
+            square.children[0].remove();
+    }
+}
+const endGame=(victory,draw)=>{
+ isLock=true;
+ const playerStatus=getPlayerStatus(getIsWhiteTurn()?'red':'white');
+ yesButton.classList.add('hidden');
+ noButton.innerHTML='close';
+  modalContainer.classList.remove('hidden');
+  modal.classList.remove('hidden');
+  if(victory){
+  modalMessage.innerHTML=`The Game is over! <br> The ${getIsWhiteTurn()?'white':'red'} player has won<br>`;
+  }
+else if(draw)
+     modalMessage.innerHTML='The Game ended in a Draw';
+}
+//set the buttons
+buttonDraw.addEventListener('click',()=>{
+    modalContainer.classList.remove('hidden');
+    modal.classList.remove('hidden');
+    modalMessage.innerHTML=`The ${getIsWhiteTurn()?'white':'red'} player ask for draw, do you agree?`;
+    modalMission='draw';
+})
+buttonResign.addEventListener('click',()=>{
+    modalContainer.classList.remove('hidden');
+    modal.classList.remove('hidden');
+    modalMessage.innerHTML=`Do you want to resign and end the game?`;
+    modalMission='resign';
+})
+noButton.addEventListener('click',(event)=>{
+    event.stopPropagation();
+    modalContainer.classList.add('hidden');
+    modal.classList.add('hidden');
+})
+yesButton.addEventListener('click',(event)=>{
+    event.stopPropagation();
+    modalContainer.classList.add('hidden');
+    modal.classList.add('hidden');
+    switchTurn();
+    if(modalMission==='resign')
+        endGame(true,false);
+    else if(modalMission==='draw')
+        endGame(false,true);
+})
